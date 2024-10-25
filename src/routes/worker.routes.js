8f76b3ad02d5ebee6d9a3aa4bdff554ec015971b
anicolaspc <a.nicolaspc@hotmail.com>
2024-10-24 21:27:04 -0500

    prueba

--- src/routes/worker.routes.js
+++ src/routes/worker.routes.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {
     listarTrabajadores,
-    crearTrabajador,
-    actualizarTrabajador,
-    eliminarTrabajador
+    // crearTrabajador,
+    // actualizarTrabajador,
+    // eliminarTrabajador
 } = require('../controllers/worker.controller');
-const multerUpload = require('../middleware/multer')
-const authMiddleware = require('../middleware/auth.middleware')
+// const multerUpload = require('../middleware/multer')
+// const authMiddleware = require('../middleware/auth.middleware')
 
 router.get('/', listarTrabajadores);
-router.post('/', authMiddleware, multerUpload.single('file'), crearTrabajador);
-router.put('/:mobile', authMiddleware, multerUpload.single('file'), actualizarTrabajador);
-router.delete('/:mobile', authMiddleware, eliminarTrabajador); 
+// router.post('/', authMiddleware, multerUpload.single('file'), crearTrabajador);
+// router.put('/:mobile', authMiddleware, multerUpload.single('file'), actualizarTrabajador);
+// router.delete('/:mobile', authMiddleware, eliminarTrabajador); 
 
 module.exports = router;
