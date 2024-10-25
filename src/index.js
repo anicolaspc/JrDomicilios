bd9ca06f2e562077f5c0badac678330cccefd6a3
anicolaspc <a.nicolaspc@hotmail.com>
2024-10-24 19:29:21 -0500

    index

--- src/index.js
+++ src/index.js
@@ -17,9 +17,9 @@ connectDB();
 
 app.use('/api/workers', workerRouter);
 app.use('/api/auth', authRouter)
-app.use('/api/files', fileServer(urlFile))
+app.use('/api/files', fileServer(urlFile)) 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`);
+    console.log(`Servidor corriendo en el puerto ${PORT}`); 
 });
