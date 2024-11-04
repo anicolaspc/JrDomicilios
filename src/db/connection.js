058122e434791417ed1301209c2eb46751d46d59
anicolaspc <a.nicolaspc@hotmail.com>
2024-11-04 15:43:39 -0500

    actualizacion

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI)
         console.log('Conectado a MongoDB');
     } catch (err) {
         console.error(err);
