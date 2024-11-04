ef2c25a98575a211965b504b9a0591636516870a
anicolaspc <a.nicolaspc@hotmail.com>
2024-11-04 16:06:33 -0500

    actualizacion

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ connectDB();
 app.use('/api/workers', workerRouter);
 app.use('/api/auth', authRouter)
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto ${PORT}`); 
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Servidor corriendo en el puerto ${PORT}`); 
+// });
+
+module.exports = app;
